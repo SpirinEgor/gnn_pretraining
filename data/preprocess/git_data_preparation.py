4aaaf766dce4a66f1f9b98b82f19e6d78c6b31c9
Yaroslav Sokolov <sokolov.yas@gmail.com>
2021-06-09 15:17:46 +0300

    Add raw data preparation

---
+++ data/preprocess/git_data_preparation.py
@@ -0,0 +1,184 @@
+import collections
+import glob
+import json
+import os
+import random
+import re
+from typing import Tuple, Iterator, List
+
+from tqdm.auto import tqdm
+
+Example = collections.namedtuple("Example", ["language", "project_name", "file_name", "source_code"])
+
+_DEFAULT_STATS_BOUNDARIES = {
+    "Python": {"max_line_len": (37, 741), "content_len": (111, 42476)},
+    "Java": {"max_line_len": (56, 177), "content_len": (305, 48661)},
+    "Kotlin": {"max_line_len": (25, 158), "content_len": (69, 20402)},
+}
+_BAD_TEXT_REGEX = re.compile(r"auto[- ]?generated file", flags=re.IGNORECASE)
+
+
+class GitProjectExtractor:
+    def __init__(self, raw_data_path: str, random_seed: int, val_part: float, test_part: float):
+        self._path = raw_data_path
+        self._rng = random.Random(random_seed)
+
+        self._val_part = val_part if val_part is not None else 0.0
+        self._test_part = test_part if test_part is not None else 0.0
+        assert self._val_part + self._test_part <= 1.0
+        self._train_part = 1.0 - self._val_part - self._test_part
+
+        self._found_projects_amount = 0
+        self._processed_projects = None
+
+        self._skipped_files = []
+
+    # Main method
+    def get_examples(self, holdout: str, languages: Tuple[str] = ("Python",)) -> Iterator[Example]:
+        """Read all files in specified language from dataset and return a project iterator"
+
+        :param raw_data_path: path to the "dataset/v3" directory
+        :param holdout: which holdout to return. Can be either "train", "val" and "test"
+        :param languages: programming languages, projects in which will be searched
+        :return: Iterator, which returns projects - Lists of Tuples, each of which represent project's files
+        """
+        if self._processed_projects is None:
+            print(f"Extracting projects metainfo...")
+            self._extract_projects(languages)
+        return self._generate_examples_iter(holdout)
+
+    # -------------------------------------- Stage methods -------------------------------------- #
+    def _extract_projects(self, languages: Tuple[str]):
+        lang_files = self._get_lang_files(languages)
+        projects = self._get_files_projects(lang_files)
+        self._found_projects_amount = len(projects)
+        processed_projects, skipped_projects, found_files_amount = self._process_projects(projects)
+
+        self._processed_projects = dict()
+        self._rng.shuffle(processed_projects)
+        train_projects_amount = int(self._train_part * len(processed_projects))
+        val_projects_amount = int(self._val_part * len(processed_projects))
+        self._processed_projects["train"] = processed_projects[:train_projects_amount]
+        self._processed_projects["val"] = processed_projects[
+            train_projects_amount : train_projects_amount + val_projects_amount
+        ]
+        self._processed_projects["test"] = processed_projects[train_projects_amount + val_projects_amount :]
+
+        tqdm.write(
+            f"Found {self._found_projects_amount} projects with {found_files_amount} files, "
+            f"skipped {len(skipped_projects)} projects\n"
+        )
+        if len(skipped_projects) != 0:
+            tqdm.write(f"Skipped projects: {skipped_projects}\n")
+
+    def _generate_examples_iter(self, holdout: str) -> Iterator[Example]:
+        """Yield all project files, one project at a time"""
+
+        def read_file(path):
+            with open(path, "rt", encoding="utf-8", errors="ignore") as f:
+                return f.read()
+
+        for project in tqdm(self._processed_projects[holdout], desc=f"Reading {holdout} projects..."):
+            examples = (
+                Example(language, proj_name, filename, read_file(path))
+                for language, proj_name, filename, path in project
+            )
+            yield from (
+                example
+                for example in examples
+                if GitProjectExtractor._is_good_example(example.language, example.file_name, example.source_code)
+            )
+
+    @staticmethod
+    def _is_good_example(language: str, filename: str, source_code: str) -> bool:
+        if not filename or not source_code:
+            return False
+
+        # Check stats
+        if not (
+            _DEFAULT_STATS_BOUNDARIES[language]["content_len"][0]
+            <= len(source_code)
+            <= _DEFAULT_STATS_BOUNDARIES[language]["content_len"][1]
+            and _DEFAULT_STATS_BOUNDARIES[language]["max_line_len"][0]
+            <= max(len(line) for line in source_code.split("\n"))
+            <= _DEFAULT_STATS_BOUNDARIES[language]["max_line_len"][1]
+        ):
+            return False
+
+        # Regex check
+        if re.search(_BAD_TEXT_REGEX, source_code):
+            return False
+
+        return True
+
+    # --------------------------------- Paths processing methods -------------------------------- #
+    def _get_lang_files(self, languages: Tuple[str]) -> List[Tuple[str, str]]:
+        res = []
+        for language in languages:
+            lang_files = glob.glob(
+                os.path.join(self._path, "languages", language, ".*", "*", "*", "**", "*.*"), recursive=True
+            )
+            assert lang_files, f"There are no files in {self._path} with language {language}"
+            print(f"Found {len(lang_files)} files' metainfos for {language} lang")
+            res.extend((lang_file, language) for lang_file in lang_files)
+        return res
+
+    @staticmethod
+    def _get_files_projects(lang_files: List[Tuple[str, str]]) -> List[Tuple[str, List[Tuple[str, str]]]]:
+        """Group all files by projects"""
+        projects = collections.defaultdict(list)
+        for (file, lang) in lang_files:
+            if os.path.isfile(file):
+                project_name = os.sep.join(file.split(os.sep)[-3:-1])
+                projects[project_name].append((file, lang))
+
+        projects = list(projects.items())
+        return projects
+
+    def _process_projects(
+        self, projects: List[Tuple[str, List[Tuple[str, str]]]]
+    ) -> Tuple[List[List[Tuple[str, str, str, str]]], List[str], int]:
+        """Search for projects, extract real project names from dataset
+
+        :param raw_data_path: path to the "dataset/v3" directory
+        :param projects: output of _get_files_projects.
+
+        :return: a Tuple,
+            first item of which is a List, each item of which represents a single GitHub project
+                and is itself a List, each item of which represents a single file in the project
+                which is written in the specified language
+                and is itself a Tuple, first item of which is the path to a file in the project structure,
+                the second one is the path to the file in our dataset structure
+                the third one is the language of the file.
+            second item is the length of projects list.
+        """
+        processed_projects = []
+        skipped_projects = []
+        files_amount = 0
+        for project_name, files in projects:
+            author, repo, branch, filename = files[0][0].split(os.sep)[-4:]
+            paths_dict_path = os.path.join(
+                self._path,
+                "repositories",
+                author,
+                repo,
+                branch,
+                "paths.json",
+            )
+            if os.path.exists(paths_dict_path):
+                with open(paths_dict_path, "rt") as f:
+                    paths_dict = json.load(f)
+
+                names_and_paths = []
+                for (file, lang) in files:
+                    if os.path.basename(file) in paths_dict:
+                        names_and_paths.append((lang, project_name, paths_dict[os.path.basename(file)], file))
+                    else:
+                        self._skipped_files.append((file, os.stat(file).st_size))
+
+                processed_projects.append(names_and_paths)
+                files_amount += len(names_and_paths)
+            else:
+                skipped_projects.append(f"{author}/{repo}")
+
+        return processed_projects, skipped_projects, files_amount
--- data/preprocess/preprocess.py
+++ data/preprocess/preprocess.py
@@ -1,19 +1,19 @@
+import itertools
+import os
 import json
 import logging
+import random
 from argparse import ArgumentParser
-from multiprocessing import Pool, cpu_count, Lock, Semaphore, Queue
-from os import getcwd
-from os.path import join, dirname, basename
-from typing import Dict, Optional
+from multiprocessing import Pool, cpu_count
+from typing import Dict, Optional, List, Iterable
 
 from tqdm.auto import tqdm
 
+from data.preprocess.git_data_preparation import GitProjectExtractor, Example
 from data.preprocess.typilus.graphgenerator import AstGraphGenerator
 from data.preprocess.typilus.type_lattice_generator import TypeLatticeGenerator
 
-DELIMITER_EXAMPLE = "␢"
-DELIMITER_FILENAME = "₣"
-TYPE_LATTICE_CONFIG = join(getcwd(), dirname(__file__), "typilus/type_lattice_config.json")
+TYPE_LATTICE_CONFIG = os.path.join(os.getcwd(), os.path.dirname(__file__), "typilus/type_lattice_config.json")
 
 USE_CPU = cpu_count()
 
@@ -24,48 +24,70 @@ logger = logging.getLogger(__name__)
 
 def configure_arg_parser() -> ArgumentParser:
     arg_parser = ArgumentParser()
-    arg_parser.add_argument("-d", "--data", help="Path to file with source code")
+    arg_parser.add_argument("-f", "--data", type=str, required=True, help='Path to the "dataset/v3" directory')
+    arg_parser.add_argument(
+        "-t", "--dest", type=str, required=True, help="Path to directory where graphs will be stored"
+    )
+    arg_parser.add_argument(
+        "--train_part", type=float, default=0.7, help="Part of the dataset which will be used for training"
+    )
+    arg_parser.add_argument(
+        "--val_part", type=float, default=0.2, help="Part of the dataset which will be used for validation"
+    )
+    arg_parser.add_argument(
+        "--test_part", type=float, default=0.2, help="Part of the dataset which will be used for testing"
+    )
+    arg_parser.add_argument("--seed", type=int, default=17, help="Random seed for projects and examples shuffle")
+
     return arg_parser
 
 
-def extract_graph(file_name: str, source_code: str) -> Optional[Dict]:
+def extract_graphs(example: Example) -> Optional[Dict]:
     type_lattice = TypeLatticeGenerator(TYPE_LATTICE_CONFIG)
     try:
-        visitor = AstGraphGenerator(source_code, type_lattice)
+        visitor = AstGraphGenerator(example.source_code, type_lattice)
         graph = visitor.build()
     except Exception as e:
         # TODO: proper logging in multiprocessing
-        logging.error(f"Can't generate graph from {file_name}, exception: {e}")
+        logging.error(f"Can't generate graph from {example.file_name}, exception: {e}")
         return None
-    if graph is None or len(graph["supernodes"]) == 0:
-        logging.error(f"Found empty graph for {file_name}")
-        return None
-    graph["filename"] = file_name
+
+    graph["file_name"] = example.file_name
+    graph["project_name"] = example.project_name
+
     return graph
 
 
-def preprocess(data_path: str):
-    with open(data_path, encoding="utf-8", errors="ignore") as f:
-        examples = [
-            tuple(map(lambda x: x.strip(), e.split(DELIMITER_FILENAME))) for e in f.read().split(DELIMITER_EXAMPLE) if e
-        ]
-
+def process_holdout(
+    data_extractor: GitProjectExtractor, rng: Optional[random.Random], holdout: str, dest_path: str
+) -> None:
     with Pool(USE_CPU) as pool:
-        graphs = pool.starmap(extract_graph, tqdm(examples))
-    graphs = [g for g in graphs if g is not None]
-    print(f"Extract {len(graphs)} from {len(examples)} source files")
+        project_graphs = pool.map(extract_graphs, data_extractor.get_examples(holdout))
+    # project_graphs = map(extract_graphs, data_extractor.get_examples(holdout))
+    graphs = [g for graphs in project_graphs for g in graphs if g is not None]
+    if rng:
+        rng.shuffle(graphs)
 
-    data_folder = dirname(data_path)
-    data_name = basename(data_path).split(".")[0]
-    output_file = join(data_folder, data_name + ".jsonl")
+    os.makedirs(dest_path, exist_ok=True)
+    output_file = os.path.join(dest_path, f"graphs_{holdout}.jsonl")
     print(f"Saving graph in JSONL format to {output_file}")
     with open(output_file, "w") as out:
-        for graph in tqdm(graphs):
-            out.write(json.dumps(graph) + "\n")
+        out.writelines(json.dumps(graph) for graph in tqdm(graphs))
+
+
+def preprocess(data_path: str, dest_path: str, random_seed: int, val_part: Optional[float], test_part: Optional[float]):
+    data_extractor = GitProjectExtractor(data_path, random_seed, val_part, test_part)
+    rng = random.Random(random_seed)
+
+    process_holdout(data_extractor, rng, "train", dest_path)
+    if val_part:
+        process_holdout(data_extractor, rng, "val", dest_path)
+    if test_part:
+        process_holdout(data_extractor, rng, "test", dest_path)
 
 
 if __name__ == "__main__":
     parser = configure_arg_parser()
     args = parser.parse_args()
 
-    preprocess(args.data)
+    preprocess(args.data, args.dest, random_seed=args.seed, val_part=args.val_part, test_part=args.test_part)
